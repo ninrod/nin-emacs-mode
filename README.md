162617af52e1dea1b0ba80d3083cc2c660f045bb
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-02-23 17:30:27 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+NIN-EMACS-MODE
+=======
+
+This plugin fixes some keyboard issues with zsh. Emacs mode has a least surprise factor over vi-mode. Use this for non-vi oriented people.
---
+++ nin-emacs-mode.plugin.zsh
@@ -0,0 +1,22 @@
+# Author: Filipe Silva (ninrod)
+# LICENSE: GPLv3
+
+# emacs mode is less surprising to new shell users
+bindkey -e
+
+############# zsh escape code fixes #############
+# home key
+bindkey "^[[1~" beginning-of-line
+
+# end key
+bindkey "^[[4~" end-of-line
+
+# delete key
+bindkey "^[[3~" delete-char
+
+# backspace key
+bindkey "^H" backward-delete-char
+bindkey "^?" backward-delete-char
+
+# numeric keypad return (enter)
+bindkey "${terminfo[kent]}" accept-line
