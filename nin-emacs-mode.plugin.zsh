4e9026ccfa62aef54f3dffcb2126f252d77bb397
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-04-22 19:31:55 -0300

    remove jump words bind

--- nin-emacs-mode.plugin.zsh
+++ nin-emacs-mode.plugin.zsh
@@ -18,15 +18,12 @@ bindkey "^[[3~" delete-char
 bindkey "^H" backward-delete-char
 bindkey "^?" backward-delete-char
 
+# numeric keypad fixes
+
 # numeric keypad return (enter)
 bindkey "${terminfo[kent]}" accept-line
 
-# jump words
-bindkey -M emacs '^L' emacs-forward-word
-bindkey -M emacs '^H' emacs-backward-word
-
-# Keypad
-# 0 .
+# `0` and `.`
 bindkey -s "^[Op" "0"
 bindkey -s "^[On" "."
 
@@ -34,14 +31,17 @@ bindkey -s "^[On" "."
 bindkey -s "^[Oq" "1"
 bindkey -s "^[Or" "2"
 bindkey -s "^[Os" "3"
+
 # 4 5 6
 bindkey -s "^[Ot" "4"
 bindkey -s "^[Ou" "5"
 bindkey -s "^[Ov" "6"
+
 # 7 8 9
 bindkey -s "^[Ow" "7"
 bindkey -s "^[Ox" "8"
 bindkey -s "^[Oy" "9"
+
 # + -  * /
 bindkey -s "^[Ol" "+"
 bindkey -s "^[OS" "-"
